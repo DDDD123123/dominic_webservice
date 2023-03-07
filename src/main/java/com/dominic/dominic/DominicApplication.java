706baee5f1a3f974e878559021c74aa3049688b1
jaehyeon <wogus1208@gmail.com>
2023-03-07 23:44:32 +0900

    Initial commit

---
+++ src/main/java/com/dominic/dominic/DominicApplication.java
@@ -0,0 +1,13 @@
+package com.dominic.dominic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DominicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dominic/dominic/DominicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dominic.dominic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
